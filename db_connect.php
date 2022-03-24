dc37589e6d81c77da7a58d970ad4d309e4ac4416
inscute08 <60645995+inscute08@users.noreply.github.com>
2022-03-24 17:49:00 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -12,10 +12,10 @@ class DB_CONNECT {
      */
     function connect() {
 
-    define('DB_USER', "root"); // db user
-    define('DB_PASSWORD', "test"); // db password (mention your db password here)
-    define('DB_DATABASE', "brgy_system"); // database name
-    define('DB_SERVER', "localhost"); // db server
+    define('DB_USER', "b97ac2cc002c36"); // db user
+    define('DB_PASSWORD', "19b6b88b"); // db password (mention your db password here)
+    define('DB_DATABASE', "heroku_d738d2345bbb9d5"); // database name
+    define('DB_SERVER', "us-cdbr-east-05.cleardb.net"); // db server
 
 // import database connection variables
         //require_once __DIR__ . '/db_config.php';
