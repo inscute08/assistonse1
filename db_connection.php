34bc829adf8f5049025a17ff6ba36158368180d4
inscute08 <60645995+inscute08@users.noreply.github.com>
2022-03-24 17:47:49 +0800

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,8 +1,8 @@
 <?php
-    define("DB_HOST","localhost");
-    define("DB_USER","root");  
-    define("DB_PASSWORD","test");
-    define("DB_DATABASE","brgy_system");
+    define("DB_HOST","us-cdbr-east-05.cleardb.net");
+    define("DB_USER","b97ac2cc002c36");  
+    define("DB_PASSWORD","19b6b88b");
+    define("DB_DATABASE","heroku_d738d2345bbb9d5");
 
     $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
 
