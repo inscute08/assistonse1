c18e8e31212549046bf495f1fb109d55f4170edc
inscute08 <60645995+inscute08@users.noreply.github.com>
2022-04-01 00:45:15 +0800

    Update sendMsg.php

--- sendMsg.php
+++ sendMsg.php
@@ -1,15 +1,11 @@
 <?php
     require "connect.php";
 
-    #$type = $_POST['type'];
-    #$user = $_POST['user'];
-    #$msg = $_POST['msg'];
+    $type = $_POST['type'];
+    $user = $_POST['user'];
+    $msg = $_POST['msg'];
     #$date = date('y-m-d h:i:s');
 
-
-    $type = "LOGBOOK";
-    $user = "test1";
-    $msg = "testingsss";
     
     $sql_query = "INSERT INTO messages (to_id, from_id, user_type, message, status) values ('20118989', '20118980', '$type', '$msg', '1');";
     mysqli_query($con,$sql_query);
