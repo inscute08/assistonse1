45077d0aa51cca0f4c10202d454da02622ae0ead
inscute08 <60645995+inscute08@users.noreply.github.com>
2022-04-22 02:38:11 +0800

    Update sendMsg.php

--- sendMsg.php
+++ sendMsg.php
@@ -6,6 +6,16 @@
     $msg = $_POST['msg'];
     #$date = date('y-m-d h:i:s');
 
+    $sql_query = "select * from seniors where UserID like '$user';";
+    $result = mysqli_query($con, $sql_query);
+    if(mysqli_num_rows($result) > 0){
+        while($row = mysqli_fetch_assoc($result)){
+                $hello = $row['SeniorID'];
+           }
+    }
+    else {
+        echo "Cant find user ID";
+    }
     
     $sql_query = "INSERT INTO messages (to_id, from_id, user_type, message, status) values ('1006', '$user', '$type', '$msg', '1');";
     mysqli_query($con,$sql_query);
